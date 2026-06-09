fd239ee86c1a57bef44d69920d2eef683d5435f3
Dexen <emiliocaballero@ciencias.unam.mx>
2026-06-09 16:59:30 -0600

    Se ajusto el codigo para hacerlo java 14

--- controlFlujo/src/opciones/OperacionesAritmeticas.java
+++ controlFlujo/src/opciones/OperacionesAritmeticas.java
@@ -16,7 +16,28 @@ public class OperacionesAritmeticas {
         System.out.println("Ingresa un operador para a y b (+,-,*,/,%)");
         operador = sc.next().charAt(0);
         sc.close();
-        switch (operador) {
+        Integer res = switch(operador){
+            case '+' -> a+b;
+            case '-' -> a-b;
+            case '*' -> a*b;
+            case '/' -> {
+                if (b==0) {
+                    System.out.println("b = 0, operacion no valida");
+                    yield null;
+                }
+                yield a/b;
+            }
+            case '%' -> {
+                if (b==0) {
+                    System.out.println("b = 0, operacion no valida");
+                    yield null;
+                }
+                yield a%b;
+            }
+            default -> null;
+        };
+        System.out.println(a+" "+operador+" "+b+" = "+res);
+        /*switch (operador) {
             case '+' -> System.out.println("a + b = "+(a+b));
             case '-' -> System.out.println("a - b = "+(a-b));
             case '*' -> System.out.println("a * b = "+(a*b));
@@ -32,7 +53,8 @@ public class OperacionesAritmeticas {
             }}
         
             default -> System.out.println("operador no disponible");
-        }
+            
+        }*/
 
     }
 }
