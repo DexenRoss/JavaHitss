a1a092f066a3702f008d8870732ec6c88b595339
Dexen <emiliocaballero@ciencias.unam.mx>
2026-07-01 16:54:20 -0600

    Vimos Polimorfismo o sobrecarga

---
+++ payment/polimorfismo/src/classes/Calculator.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Calculator {
+    public int suma(int a, int b){
+        return a+b;
+    }
+
+    public double suma(double a, int b){
+        return a+b;
+    }
+
+    public long suma(long a, long b){
+        return a+b;
+    }
+
+    public int suma(int a, int b, int c){
+        return a+b+c;
+    }
+}
---
+++ payment/polimorfismo/src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import classes.Calculator;
+
+public class Test {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.suma(23, 34);
+        calculator.suma(3454543465765L, 5L);
+    }
+}
