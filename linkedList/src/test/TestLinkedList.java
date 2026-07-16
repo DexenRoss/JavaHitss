7e0e5fa06c0d4e0c3e27cfc7b14723546256cb80
Dexen <emiliocaballero@ciencias.unam.mx>
2026-07-16 16:57:48 -0600

    Se inicio a ver Linked List

---
+++ linkedList/src/test/TestLinkedList.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestLinkedList {
+    public static void main(String[] args) {
+        List<String> names = new LinkedList<>();
+        names.add("Emilio");
+        names.add("Juan");
+        names.add("null");
+    }
+}
