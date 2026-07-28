e5524d5fb06e9f342257b35ff88b0b0f6655f2fa
Dexen <emiliocaballero@ciencias.unam.mx>
2026-07-28 16:15:00 -0600

    vimos Map

--- mapas/src/App.java
+++ mapas/src/App.java
@@ -1,5 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        String name = "Javac 1";
+        System.out.println(name.hashCode());
+        Map<Integer,String> names = new HashMap<>();
+        names.put(21, "Emilio");
+        names.put(21, "Dexen");
+        names.put(12, "Liz");
+        names.put(14, "Mar");
+        System.out.println(names);
+
+        System.out.println(names.get(21));
+        if (names.containsKey(21)) {
+            System.out.println(names.remove(21));
+        }
+        
+        System.out.println("key? "+names.containsKey(14));
+        System.out.println("Valor: "+names.containsValue("Liz"));
     }
 }
